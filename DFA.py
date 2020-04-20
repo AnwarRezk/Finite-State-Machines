37a334f5934f7cc906c43880c7b38d9a99b2885f
Anwar Rezk <33953443+AnwarRezk@users.noreply.github.com>
2020-04-20 20:26:26 +0200

    Update DFA.py

--- DFA.py
+++ DFA.py
@@ -1,5 +1,5 @@
 class DFA:
-    #DFA is a 5-tuples
+    #DFA is a 5-tuple
     def __init__(self,finite_states,alphabet,transition_function,initial_state,final_states):
         self.finite_states = finite_states 
         self.alphabet = alphabet
@@ -22,27 +22,27 @@ class DFA:
 if __name__ == "__main__":
     
     #Initialize DFA parameters
-    K = [0,1,2]
+    K = ['S0','S1','S2']
     A = ['a','b']
     TF = {}
-    TF[(0,'a')] = 2
-    TF[(0,'b')] = 0
-    TF[(1,'a')] = 1
-    TF[(1,'b')] = 2
-    TF[(2,'a')] = 1
-    TF[(2,'b')] = 0
-    S = 1
-    F = [0,2]
+    TF[('S0','a')] = 'S2'
+    TF[('S0','b')] = 'S0'
+    TF[('S1','a')] = 'S1'
+    TF[('S1','b')] = 'S2'
+    TF[('S2','a')] = 'S1'
+    TF[('S2','b')] = 'S0'
+    S = 'S1'
+    F = ['S0','S2']
 
     M = DFA(K,A,TF,S,F)
     try:
         print("Enter a String : ",end='')
-        inp_str = input()
-        for i in inp_str:
+        tape = input()
+        for i in tape:
             if i not in A:
                 raise NameError
                 break
     except NameError:
         print("The String entered is not valid!")
     else:
-        M.check_str_validity(inp_str)
+        M.check_str_validity(tape)
